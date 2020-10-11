82a7945bdac6ee176b8cdf64b2e2694190535e2d
NenadDjuricic <shone1995@gmail.com>
2020-10-11 12:37:14 +0200

    Postavljanje sajta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,30 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-sass','gatsby-plugin-styled-components'],
+  siteMetadata:{
+    title: "Crystalweed Srbija",
+    description:"Najbolji proizvodi od cistog cbd-a, cbd kreme, cbd cajevi. Prirodna medicina, zategnite lice i osecajte se srecno i ispunjeno uz najbolje CBD proizvode.",
+    author: "Nenad Djuricic",
+    image:"/CbdProducts.jpeg",
+    siteUrl:""
+  },
+  plugins: [{
+    resolve: 'gatsby-source-filesystem',
+    options:{
+      name:'images',
+      path: `${__dirname}/src/images`,
+    },
+  },
+  {
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: process.env.CONTENTUFUL_SPACE_ID,
+      // Learn about environment variables: https://gatsby.dev/env-vars
+      accessToken: process.env.CONTENTUFUL_ACCES_TOKEN,
+    },
+  },
+    'gatsby-plugin-sass','gatsby-plugin-styled-components',`gatsby-transformer-sharp`, `gatsby-plugin-sharp`,'gatsby-plugin-transition-link',`gatsby-plugin-playground`,`gatsby-plugin-react-helmet],
 }
+ 
